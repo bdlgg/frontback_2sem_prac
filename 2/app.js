360d02f3f79759b8dc7a46eb344896f189b5dd6a
aleksejkonstantinov <40akka@gmail.com>
2026-02-28 10:07:25 +0300

    5 практика, добавлен swagger

--- 2/app.js
+++ 2/app.js
@@ -3,7 +3,8 @@ const {nanoid} = require('nanoid');
 const app = express();
 const port = 3000;
 const cors = require('cors');
-
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 let items = [
     {id: nanoid(6), name: "Iphone", price: 70000, description: "Прекрасный айфон", category: "Электроника", stock: 5},
     {id: nanoid(6), name: "Zphone", price: 100000, description: "Непрекрасный айфон", category: "Электроника", stock: 10},
@@ -36,6 +37,66 @@ app.use((req,res,next)=>{
     });
     next();
 });
+//swagger definition
+//описание основного api
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Api управления товарами',
+            version: '1.0.0',
+            description: 'простое API для управления товарами'
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`,
+                description: 'Локальный хост'
+            }
+        ]
+    },
+    //путь к файлам в которых будем писать jsdoc-комментарий (текущий файл)
+    apis: ['./app.js'],
+}
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+//подключаем swagger ui по адресу /api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Item:
+ *       type: object
+ *       required:
+ *         - name
+ *         - price
+ *         - stock
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: Автоматически сгенерированный уникальный ID товара
+ *         name:
+ *           type: string
+ *           description: Имя Товара
+ *         price:
+ *           type: integer
+ *           description: Стоимость товара
+ *         stock:
+ *           type: integer
+ *           description: Остаток на складе
+ *         category:
+ *           type: string
+ *           description: Категория товара
+ *         description:
+ *           type: string
+ *           description: Описание товара
+ *       example:
+ *         id: "abc123"
+ *         name: "Maxphone"
+ *         price: 1000
+ *         stock: 1
+ *         category: "Электроника"
+ *         description: "Отличный товар"
+ */
 
 //функция помощник для получения пользователя из списка
 function findItemOr404(id, res) {
@@ -50,8 +111,50 @@ function findItemOr404(id, res) {
 }
 
 //CRUD
+/**
+ * @swagger
+ * /api/items:
+ *   post:
+ *     summary: Создает новый товар
+ *     tags: [Items]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - price
+ *               - stock
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: integer
+ *               stock:
+ *                 type: integer
+ *               category:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Товар успешно создан
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Item'
+ *       400:
+ *         description: Ошибка в теле запроса
+ */
 app.post('/api/items', (req, res) => {
     const {name, category, description, price, stock} = req.body;
+    if (!name || !price || stock === undefined){
+        return res.status(400).json({
+            error: "Имя или цена обязательные, остаток тоже, хоть 0"
+        })
+    }
     const newItem = {
         id: nanoid(6),
         name: name.trim(),
@@ -64,10 +167,50 @@ app.post('/api/items', (req, res) => {
     res.status(201).json(newItem);
 });
 
+/**
+ * @swagger
+ * /api/items:
+ *   get:
+ *     summary: Возвращает список всех товаров
+ *     tags: [Items]
+ *     responses:
+ *       200:
+ *         description: Список товаров
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Item'
+ */
+
 app.get("/api/items", (req, res) => {
     res.json(items);
 })
 
+/**
+ * @swagger
+ * /api/items/{id}:
+ *   get:
+ *     summary: Получает товар по ID
+ *     tags: [Items]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID товара
+ *     responses:
+ *       200:
+ *         description: Данные товара
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Item'
+ *       404:
+ *         description: Товар не найден
+ */
 app.get("/api/items/:id", (req, res) => {
     const id = req.params.id;
     const item = findItemOr404(id, res);
@@ -75,11 +218,55 @@ app.get("/api/items/:id", (req, res) => {
     res.json(item);
 });
 
+
+/**
+ * @swagger
+ * /api/items/{id}:
+ *   patch:
+ *     summary: Обновляет данные товара
+ *     tags: [Items]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID товара
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: integer
+ *               stock:
+ *                 type: integer
+ *               category:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Обновленный товар
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Item'
+ *       400:
+ *         description: Нет данных для обновления
+ *       404:
+ *         description: Товар не найден
+ */
 app.patch('/api/items/:id', (req, res) => {
     const id = req.params.id;
     const item = findItemOr404(id, res);
     if (!item) return;
 
+    //Нельзя patch без полей
     if (req.body?.name === undefined && req.body?.price === undefined && req.body?.description === undefined && req.body?.category === undefined && req.body?.stock === undefined) {
         res.status(404).json({
             error: 'Обновить нечего, сори'
@@ -94,6 +281,25 @@ app.patch('/api/items/:id', (req, res) => {
     res.json(item);
 });
 
+/**
+ * @swagger
+ * /api/items/{id}:
+ *   delete:
+ *     summary: Удаляет товар
+ *     tags: [Items]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID Товара
+ *     responses:
+ *       204:
+ *         description: Товар успешно удален (нет тела ответа)
+ *       404:
+ *         description: Товар не найден
+ */
 app.delete('/api/items/:id', (req, res) => {
     const id = req.params.id;
     const exists = items.some((i) => i.id === id);
